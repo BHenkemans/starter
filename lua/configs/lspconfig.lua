ffd00367cb55444de13a932c66027d1fa4d2c015
Bartjan Henkemans <bartjan@henkemans.be>
2026-03-17 21:18:30 +0100

    Made harper detect tex files (amongst other things)

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,12 @@
 require("nvchad.configs.lspconfig").defaults()
 
 local servers = { "html", "cssls", "harper_ls" }
+
+vim.lsp.config.harper_ls = {
+  -- Tell Harper exactly which files to attach to
+  filetypes = { "tex", "plaintex", "markdown", "gitcommit" }, 
+}
+
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
