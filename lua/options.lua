9264a8e687de3e5313f98dff28e2db870c7362de
Bartjan Henkemans <bartjan@henkemans.be>
2026-03-17 21:21:39 +0100

    text wrap

--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,8 @@
 require "nvchad.options"
 
--- add yours here!
+local opt = vim.opt
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+--  Text wrap
+opt.wrap = true
+opt.linebreak = true
+opt.breakindent = true
